e56fa7e6838ee80d49f8c8fde4c7e60d2bb4cec3
Alex5 <Alex5@TOLEDO521>
2023-10-28 15:45:44 -0600

    Agregar archivos de proyecto.

---
+++ TABLAS DE MULTIPLICAR CON WHILW 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TABLA_DE_MULTIPLICAR_CON_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, RESUL, T, I;
+            string linea;
+            Console.Write("CUANTAS TABLAS: "); linea = Console.ReadLine();
+            NUM = int.Parse (linea);
+            T = 1;
+            while((T <= NUM))
+            {
+                I = 10;
+                while(!((I < 1)))
+                {
+                    RESUL = T * I;
+                    Console.WriteLine("{0} * {1} = {2}", T, I, RESUL);
+                    I = I - 1;
+                }
+                Console.Write("PULSE UNA TECLA: "); Console.ReadLine();
+                T = T + 1;
+            }
+        }
+    }
+}
